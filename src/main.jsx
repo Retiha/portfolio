a0478a7f8a785d52b77dde6278cbf7d2859babac
Retiha <retiha.c@gmail.com>
2025-10-23 16:07:24 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="https://retiha.github.io/portfolio/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
