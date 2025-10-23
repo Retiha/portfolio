8f7f33a0ae27829971aa9d28f32cb0c2046a8a09
Retiha <retiha.c@gmail.com>
2025-10-23 16:23:58 +0530

    main changed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://retiha.github.io/portfolio/",   // ← add this line
   plugins: [react()],
 });
